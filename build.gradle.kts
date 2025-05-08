a158b64b1d78cfd10db29ba7050044cff0dcada6
4win-official <mohammad.shenavande@gmail.com>
2025-05-09 02:33:28 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,19 @@
-plugins {
-    id("com.android.application")
-    kotlin("android")
-}
+// build.gradle.kts (project-level)
 
-android {
-    namespace = "com.example.namespace" // جایگزین کن با نام‌دلخواه مثل com.fourwin.vpn
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.namespace" // همون مقدار namespace
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0") // برای دریافت JSON از گیت‌هاب
-}
+rootProject.name = "v2rayng"
+include(":app")
