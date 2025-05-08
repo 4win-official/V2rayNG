c8a5a5a27f84b68251c87fb0d48b1bf4d49bed6d
4win-official <mohammad.shenavande@gmail.com>
2025-05-09 02:19:15 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,19 @@
-plugins {
-    id("com.android.application") version "8.0.0"
-    id("org.jetbrains.kotlin.android") version "1.8.10"
-}
-
 android {
+    namespace = "com.example.namespace" // همون رو بزارید
     compileSdk = 34
+
     defaultConfig {
-        applicationId = "your.app.id"
+        applicationId = "com.example.namespace" // همون مقدار باشه
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
+
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 }
